0b4f7751b7edb724992a7cee93f7f6341e215b04
ahyom <atrpaula@gmail.com>
2023-07-16 16:39:27 -0300

    wip - saving autor correctly

--- src/main/kotlin/com/ahyom/cdc/service/AutorService.kt
+++ src/main/kotlin/com/ahyom/cdc/service/AutorService.kt
@@ -1,21 +1,22 @@
 package com.ahyom.cdc.service
 
 import com.ahyom.cdc.domain.entity.Autor
+import com.ahyom.cdc.domain.repository.AutorRepository
 import mu.KotlinLogging
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 private val logger = KotlinLogging.logger {}
 
 @Service
-class AutorService {
+class AutorService @Autowired constructor(
+    val autorRepository: AutorRepository
+) {
 
     fun createAutor(autor: Autor): Autor {
         logger.debug { "creating autor..." }
-
-        // call repository to save autor
-
+        autorRepository.save(autor)
         logger.debug { "autor created with id ${autor.id}" }
-
         return autor
     }
 }
