4b9c78022e2dcce63e533c5d2ad5eba3b41548a7
ahyom <atrpaula@gmail.com>
2023-07-16 16:32:39 -0300

    wip - add h2 database and autor entity info

--- src/main/kotlin/com/ahyom/cdc/model/entity/Autor.kt
+++ src/main/kotlin/com/ahyom/cdc/model/entity/Autor.kt
@@ -1,12 +1,28 @@
 package com.ahyom.cdc.model.entity
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 import java.time.LocalDateTime
 import java.util.UUID
 
+@Entity
+@Table(name = "tb_autor")
 class Autor(
+
+    @Id
     var id: UUID,
+
+    @Column(nullable = false, unique = false)
     var name: String,
+
+    @Column(nullable = false, unique = false)
     var email: String,
+
+    @Column(length = 400, nullable = false, unique = false)
     var description: String,
+
+    @Column(nullable = false, unique = false)
     var createdAt: LocalDateTime,
 )
