e847c76a49a529e58e67c8d3ea85ee3569abde0a
ahyom <atrpaula@gmail.com>
2023-07-16 16:58:08 -0300

    wip - fix path variables and create other crud methods

--- src/main/kotlin/com/ahyom/cdc/controller/AutorController.kt
+++ src/main/kotlin/com/ahyom/cdc/controller/AutorController.kt
@@ -5,13 +5,18 @@ import com.ahyom.cdc.domain.request.AutorRequest
 import com.ahyom.cdc.service.AutorService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
-@RequestMapping("/autor")
+@RequestMapping("autor")
 class AutorController @Autowired constructor(
     val autorService: AutorService,
     val autorMapper: AutorMapper,
@@ -24,5 +29,26 @@ class AutorController @Autowired constructor(
         val autorCreated = autorService.createAutor(autor)
 
         return ResponseEntity.ok(autorMapper.fromEntity(autorCreated))
+    }
+
+    @GetMapping
+    fun getAutors(): ResponseEntity<List<AutorRequest>> {
+        val autors = autorService.getAutors()
+        val autorRequestList = autors.map { autorMapper.fromEntity(it) }
+
+        return ResponseEntity.ok(autorRequestList)
+    }
+
+    @GetMapping("/{autor-id}")
+    fun getAutorById(@PathVariable("autor-id") autorId: String): ResponseEntity<AutorRequest> {
+        val autor = autorService.getAutorById(UUID.fromString(autorId))
+        val autorRequest = autorMapper.fromEntity(autor)
+        return ResponseEntity.ok(autorRequest)
+    }
+
+    @DeleteMapping("/{autor-id}")
+    fun deleteAutor(@PathVariable("autor-id") autorId: String): ResponseEntity<Unit> {
+        autorService.deleteAutor(UUID.fromString(autorId))
+        return ResponseEntity.ok().build()
     }
 }
--- src/main/kotlin/com/ahyom/cdc/service/AutorService.kt
+++ src/main/kotlin/com/ahyom/cdc/service/AutorService.kt
@@ -5,18 +5,37 @@ import com.ahyom.cdc.domain.repository.AutorRepository
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 private val logger = KotlinLogging.logger {}
 
 @Service
 class AutorService @Autowired constructor(
-    val autorRepository: AutorRepository
+    val autorRepository: AutorRepository,
 ) {
-
     fun createAutor(autor: Autor): Autor {
         logger.debug { "creating autor..." }
         autorRepository.save(autor)
         logger.debug { "autor created with id ${autor.id}" }
         return autor
+    }
+
+    // TODO -> update autor
+
+    fun getAutors(): List<Autor> {
+        logger.debug { "getting autors..." }
+        return autorRepository.findAll().toList()
+    }
+
+    fun getAutorById(autorId: UUID): Autor {
+        logger.debug { "getting autor by id $autorId" }
+        // TODO -> should return a 404 if not found
+        return autorRepository.findById(autorId).orElseThrow()
+    }
+
+    fun deleteAutor(autorId: UUID) {
+        logger.debug { "deleting autor by id $autorId" }
+        // TODO -> should return a 404 if not found
+        autorRepository.deleteById(autorId)
     }
 }
