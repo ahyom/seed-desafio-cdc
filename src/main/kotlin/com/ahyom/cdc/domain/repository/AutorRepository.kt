cd8a775a0bceecd3ef32d69eda0862fc77f5685c
ahyom <atrpaula@gmail.com>
2023-07-24 19:23:09 -0300

    update custom entity findBy with Optional to improve exception throws

--- src/main/kotlin/com/ahyom/cdc/domain/repository/AutorRepository.kt
+++ src/main/kotlin/com/ahyom/cdc/domain/repository/AutorRepository.kt
@@ -3,10 +3,11 @@ package com.ahyom.cdc.domain.repository
 import com.ahyom.cdc.domain.entity.Autor
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 import java.util.UUID
 
 @Repository
 interface AutorRepository : CrudRepository<Autor, UUID> {
 
-    fun findByEmail(email: String): List<Autor>
+    fun findByEmail(email: String): Optional<List<Autor>>
 }
