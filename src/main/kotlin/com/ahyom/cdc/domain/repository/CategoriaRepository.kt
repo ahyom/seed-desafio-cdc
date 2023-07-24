5467c9834a82abd36afc5d3da743bc552f4e7a74
ahyom <atrpaula@gmail.com>
2023-07-24 18:54:48 -0300

    create repository for categoria

---
+++ src/main/kotlin/com/ahyom/cdc/domain/repository/CategoriaRepository.kt
@@ -0,0 +1,12 @@
+package com.ahyom.cdc.domain.repository
+
+import com.ahyom.cdc.domain.entity.Categoria
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface CategoriaRepository : CrudRepository<Categoria, UUID> {
+
+    fun findByName(name: String): List<Categoria>
+}
