3728e49a637717c8bf557a6bde36f08117e60af4
ahyom <atrpaula@gmail.com>
2023-08-01 21:48:02 -0300

    Add spring security and fix CORS problems in a global way

--- src/main/kotlin/com/ahyom/cdc/CdcApplication.kt
+++ src/main/kotlin/com/ahyom/cdc/CdcApplication.kt
@@ -3,7 +3,7 @@ package com.ahyom.cdc
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration::class])
 class CdcApplication
 
 fun main(args: Array<String>) {
